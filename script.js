af42f1e2fb2e6361edfb1017ff5daeee5391dbca
Jacobloucks73 <jfl6196@psu.edu>
2024-02-11 03:14:39 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*window.onscroll = function() {
+    var headerImage = document.getElementById('header-image');
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      headerImage.style.opacity = '0';
+    } else {
+      headerImage.style.opacity = '1';
+    }
+  };*/
